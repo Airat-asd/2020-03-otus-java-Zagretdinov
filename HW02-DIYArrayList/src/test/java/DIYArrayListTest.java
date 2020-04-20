1f820bdfe048e8200eb3e11de293f9c8d777de3b
Airat-asd <asd0982@yandex.ru>
2020-04-20 21:45:57 +0500

    Исправление замечаний к HW02-Collection (не надо комментировать предыдущий проект)

---
+++ HW02-DIYArrayList/src/test/java/DIYArrayListTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DIYArrayListTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void testToString() {
+    }
+}
