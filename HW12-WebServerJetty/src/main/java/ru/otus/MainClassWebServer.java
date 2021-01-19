cb051bc841b4265920246a457fcb503b7a15692a
Airat-asd <asd0982@yandex.ru>
2021-01-19 23:03:17 +0500

    HW12-WebServer

--- HW12-WebServerJetty/src/main/java/ru/otus/MainClassWebServer.java
+++ HW12-WebServerJetty/src/main/java/ru/otus/MainClassWebServer.java
@@ -15,6 +15,8 @@ import ru.otus.hibernateImplementation.HibernateUtils;
 import ru.otus.hibernateImplementation.dao.UserDaoHibernate;
 import ru.otus.hibernateImplementation.sessionmanager.SessionManagerHibernate;
 
+import java.util.Collections;
+
 public class MainClassWebServer {
     private static final Logger logger = LoggerFactory.getLogger(MainClassWebServer.class);
     private static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
@@ -23,25 +25,24 @@ public class MainClassWebServer {
     private static final String TEMPLATES_DIR = "/templates/";
 
 
-
     public static void main(String[] args) {
 
         DBServiceUser dbServiceUser = createDBServiceUser();
-
-
+        dbServiceUser.saveUser(new User("Vasia", new AddressDataSet("Test_Street_1"), Collections.singletonList(new PhoneDataSet("+7894456"))));
 
     }
 
     public static DBServiceUser createDBServiceUser() {
         Configuration configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
 
-        String dbUrl = System.getProperty("app.datasource.demo-db.jdbcUrl");
-        String dbUserName = System.getProperty("app.datasource.demo-db.username");
-        String dbPassword = System.getProperty("app.datasource.demo-db.password");
+        String dbUrl = configuration.getProperty("hibernate.connection.url");
+        String dbUserName = configuration.getProperty("hibernate.connection.username");
+        String dbPassword = configuration.getProperty("hibernate.connection.password");
 
         new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
 
-        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory(configuration, User.class, AddressDataSet.class, PhoneDataSet.class);
+        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory(configuration, User.class,
+                AddressDataSet.class, PhoneDataSet.class);
 
         SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
 
